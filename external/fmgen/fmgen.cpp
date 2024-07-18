01d1214e150a93b1f48b79a58324278b1cc15a68
nullre-orz <71023895+nullre-orz@users.noreply.github.com>
2024-07-18 22:46:50 +0900

    fmgenの警告/エラー除去

--- external/fmgen/fmgen.cpp
+++ external/fmgen/fmgen.cpp
@@ -406,7 +406,7 @@ void Operator::Prepare()
 		// SSG-EG
 		if (ssg_type_ && (eg_phase_ != release))
 		{
-			int m = ar_ >= ((ssg_type_ == 8 || ssg_type_ == 12) ? 56 : 60);
+			int m = ar_ >= ((ssg_type_ == 8 || ssg_type_ == 12) ? 56u : 60u);
 
 			assert(0 <= ssg_phase_ && ssg_phase_ <= 2);
 			const int* table = ssgenvtable[ssg_type_ & 7][m][ssg_phase_];
@@ -434,7 +434,7 @@ void Operator::ShiftPhase(EGPhase nextphase)
 			if (ssg_phase_ > 2)
 				ssg_phase_ = 1;
 			
-			int m = ar_ >= ((ssg_type_ == 8 || ssg_type_ == 12) ? 56 : 60);
+			int m = ar_ >= ((ssg_type_ == 8 || ssg_type_ == 12) ? 56u : 60u);
 
 			assert(0 <= ssg_phase_ && ssg_phase_ <= 2);
 			const int* table = ssgenvtable[ssg_type_ & 7][m][ssg_phase_];
